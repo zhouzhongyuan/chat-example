46dc6fee8c4a1e047de71b21e98a410ff7f47719
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2016-11-15 15:29:57 +0800

    on connection

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
-// var io = require('socket.io')(http);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  //res.send('<h1>Hello world</h1>');
-	res.sendFile(__dirname + '/index.html');
+    res.sendfile('index.html');
 });
-// io.on('connection',function (socket) {
-//     console.log('a user connected');
-// })
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
 http.listen(5000, function(){
-  console.log('listening on *:5000');
+    console.log('listening on *:5000');
 });
